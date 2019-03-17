4ef209e2b1e6365f38bf61e2396993a507c39a9f
Asuk Kono <asukiaaa@gmail.com>
2019-03-17 20:28:15 +0900

    Add PassMega

---
+++ examples/PassMega/PassMega.ino
@@ -0,0 +1,28 @@
+#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial softSerial(8, 9);
+
+void hardHello(HardwareSerial *serial) {
+  serial->println("hello");
+}
+
+void softHello(SoftwareSerial *serial) {
+  serial->println("hello");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial2.begin(115200);
+  Serial3.begin(115200);
+  softSerial.begin(115200);
+}
+
+void loop() {
+  hardHello(&Serial);
+  hardHello(&Serial1);
+  hardHello(&Serial2);
+  hardHello(&Serial3);
+  softHello(&softSerial);
+}
