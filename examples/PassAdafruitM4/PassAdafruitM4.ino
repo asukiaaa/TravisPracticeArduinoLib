e9d878866c6567f7323948deb11e85489eb1e0ab
Asuk Kono <asukiaaa@gmail.com>
2019-03-21 19:02:27 +0900

    Change uart definition for M4

--- examples/PassAdafruitM4/PassAdafruitM4.ino
+++ examples/PassAdafruitM4/PassAdafruitM4.ino
@@ -15,16 +15,16 @@ void uartHello(Uart *serial) {
 }
 
 // https://learn.adafruit.com/using-atsamd21-sercom-to-add-more-spi-i2c-serial-ports/creating-a-new-serial
-Uart Serial2(&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
+Uart uartSerial(&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
 
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
-  Serial2.begin(115200);
+  uartSerial.begin(115200);
 }
 
 void loop() {
   usbHello(&Serial);
   hardHello(&Serial1);
-  uartHello(&Serial2);
+  uartHello(&uartSerial);
 }
